2a60e35fe9844ac1e4de2493dd3ceaf14a4cd058
Neil Campbell <neildcampbell@hotmail.com>
2019-11-15 06:04:08 +0000

    created diamond

--- spec/diamondSpec.js
+++ spec/diamondSpec.js
@@ -7,11 +7,17 @@ describe("Diamond", function(){
     it("should print B on second line", function(){
         let input = 'B'
         let result = diamond(input)
-        expect(result).toEqual('  A\n B   B')
+        expect(result).toEqual('  A\n B B')
     })
     it("should print C on third line", function(){
         let input = 'C'
         let result = diamond(input)
-        expect(result).toEqual('  A\n B   B\nC     C')
+        expect(result).toEqual('  A\n B B\nC     C')
+    })
+    it("should print D on third line and finish diamond", function(){
+        let input = 'D'
+        let result = diamond(input)
+        expect(result).toEqual('  A\n B B\nD   D\n B B\n  A')
+        console.log(result)
     })
 })
--- src/diamond.js
+++ src/diamond.js
@@ -2,8 +2,10 @@ function diamond(input){
     if(input == 'A'){
         return '  A'
     } else if (input === 'B'){
-        return '  A\n B   B'
+        return '  A\n B B'
     } else if (input === 'C'){
-        return '  A\n B   B\nC     C'
+        return `  A\n B B\n${input}     ${input}`
+    } else if(input ==='D'){
+        return `  A\n B B\n${input}   ${input}\n B B\n  A`
     }
 }
